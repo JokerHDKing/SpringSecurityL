60f13fff4dd214cdde886d05dc58a99221fc9c4d
JokerHDKing <2804183061@qq.com>
2024-03-13 22:16:37 +0800

    登录页面完成及axios封装完成

--- my-project-frontend/src/views/welcome/LoginPage.vue
+++ my-project-frontend/src/views/welcome/LoginPage.vue
@@ -1,11 +1,80 @@
+<script setup xmlns:el-col="http://www.w3.org/1999/html">
+import {User,Lock} from '@element-plus/icons-vue'
+import {reactive,ref} from "vue";
+import {login} from "@/net/index.js";
 
-<script>
+const formRef=ref()
+const  form=reactive({
+    username:"",
+    password:'',
+    remember:false
+  })
+const rule={
+    username:[
+        {required:true,message:'请输入用户名'}
+    ],
+  password:[
+    {required:true,message:'请输入密码'}
+  ]
+}
+function  userLogin(){
+  formRef.value.validate((isValid) => {
+    if (isValid){
+      login(form.username,form.password,form.remember,()=>{
 
+      })
+    }
+  });
+}
 </script>
 
 <template>
-  <div>
-    登录页面
+  <div style="text-align: center ; margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div   style="font-size:25px ;font-weight: bold">登录</div>
+      <div style="font-size: 14px;color: grey">在进入系统之前，请先输入用户名和密码进行登录</div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form :model="form" :rules="rule" ref="formRef">
+
+        <el-form-item prop="username">
+          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名/邮箱">
+              <template #prefix>
+                <el-icon><User/></el-icon>
+              </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" maxlength="20" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <el-row>
+          <el-col :span="12" style="text-align: left">
+            <el-form-item prop="remember">
+              <el-checkbox v-model="form.remember" label="记住我"/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12" style="text-align: right">
+              <el-link>忘记密码？</el-link>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div style="margin-top: 40px">
+      <el-button @click="userLogin()" style="width: 270px" type="success">立即登录</el-button>
+    </div>
+
+    <el-divider>
+      <span style="font-size:  12px;color: grey">没有账号</span>
+    </el-divider>
+    <div>
+      <el-button style=" width: 270px;" type="warning" plain>立即注册</el-button>
+    </div>
   </div>
 </template>
 
