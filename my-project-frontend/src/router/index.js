043dbaf7fda5a7bfd7c362aa5e74f720d847982d
JokerHDKing <2804183061@qq.com>
2024-03-16 00:14:28 +0800

    注册接口和注册页面的完成

--- my-project-frontend/src/router/index.js
+++ my-project-frontend/src/router/index.js
@@ -1,35 +1,39 @@
 import {createRouter, createWebHistory} from "vue-router";
 import {unauthorized} from "@/net/index.js";
 
-const router=createRouter({
-    history:createWebHistory(import.meta.env.BASE_URL),
-    routes:[
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-            path:'/',
-            name:'welcome',
-            component:() => import('@/views/WelcomeView.vue'),
-            children:[
+            path: '/',
+            name: 'welcome',
+            component: () => import('@/views/WelcomeView.vue'),
+            children: [
                 {
-                    path:'',
-                    name:'welcome-login',
+                    path: '',
+                    name: 'welcome-login',
                     component: () => import('@/views/welcome/LoginPage.vue')
+                }, {
+                    path: '/register',
+                    name: 'welcome-register',
+                    component: () => import('@/views/welcome/RegisterPage.vue')
                 }
             ]
-        },{
-            path:'/index',
-            name:'index',
-            component:() => import('@/views/IndexView.vue')
+        }, {
+            path: '/index',
+            name: 'index',
+            component: () => import('@/views/IndexView.vue')
         }
     ]
 })
-router.beforeEach((to,from,next) =>{
-    const  isUnauthorized=unauthorized()
-    if (to.name.startsWith('welcome-') && !isUnauthorized){
+router.beforeEach((to, from, next) => {
+    const isUnauthorized = unauthorized()
+    if (to.name.startsWith('welcome-') && !isUnauthorized) {
         next('/index')
-    }else if (to.fullPath.startsWith('/index') && isUnauthorized){
+    } else if (to.fullPath.startsWith('/index') && isUnauthorized) {
         next('/')
-    }else {
+    } else {
         next()
     }
 })
-export  default router
+export default router
--- my-project-frontend/vite.config.js
+++ my-project-frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import.meta.env
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -21,5 +24,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
 })
